b760aa84d9b00c2b64eda0b9109b41abcf1caaa2
kiforsbe <kim.forsberg@gmail.com>
2023-11-04 13:04:55 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# misc_cs_scripts
+# Misc Cities Skylines Scripts
 Miscellaneous Cities Skylines and Cities Skylines 2 scripts and helpers.
+
+## convert_cs1-radio_to_cs2.py
+Automatically copies and tags CS1 radio for CS2, and creates radio network and channel settings files for each supported station.
+
+### Description
+Used to automatically copy, rename and tag Cities Skylines 1 Radio tracks, prepared for use in Cities Skylines 2.
+Where the original artist is not clear, the default is to attribute Colossal Order.
+The script automatically generates Cities Skylines 2 Radio Network and Radio Channel settings files for each channel (JSON files, type ".coc").
+
+The script uses a mapping CSV file that contains the following Cities Skylines radio stations:
+| Station | Folder | DLC | Release date |
+|---------|--------|-----|--------------|
+| All That Jazz | Jazz | Paid DLC | 2017-12-05 |
+| Calm the Mind Radio | CalmTheMind | Paid DLC | 2022-01-25 |
+| Campus Radio | CampusRadio | Paid DLC | 2019-05-21 |
+| Carols, Candles and Candy | Christmas Radio | Free DLC | 2017-12-05 |
+| Classics Radio, The | Classical | Base game | 2015-03-10 |
+| Coast to Coast Radio | CoastToCoast | Paid DLC | 2020-03-26 |
+| Concerts Radio | Concerts | Paid DLC | 2017-08-17 |
+| Country Road Radio | Country | Paid DLC | 2018-05-24 |
+| Deep Focus Radio | DeepFocus | Paid DLC | 2019-05-21 |
+| Downtown Radio | DowntownRadio| Paid DLC | 2019-11-07 |
+| Gold FM | GoldFM | Base game | 2015-03-10 |
+| K-Pop Station | KPopRadio | Paid DLC | 2022-11-15 |
+| Official Mars Radio | Mars | Free in Patch 1.9.2 | 2018-03-09 |
+| On Air Radio | OnAir | Paid DLC | 2022-01-25 |
+| Paradise Radio | ParadiseRadio | Paid DLC | 2022-09-14 |
+| Rail Hawk Radio | RailHawk | Paid DLC | 2021-05-21 |
+| Relaxation Station | Relaxation | Paid DLC | 2016-11-29 |
+| Rock City Radio | Rock | Paid DLC | 2017-05-18 |
+| Shoreline Radio | ShorelineRadio | Paid DLC | 2022-09-14 |
+| Smooth Funk Radio | SmoothFunk | Base game | 2015-03-10 |
+| Sunny Breeze Radio | SunnyBreeze | Paid DLC | 2021-05-21 |
+| Synthetic Dawn Radio | Synthetic | Paid DLC | 2018-10-23 |
+
+*Request: If you have the Music Pack DLCs below and are willing to provide the data (folders & filenames) for them, that would be great!*
+
+Not supported stations (due to not having bought them yet):
+| Station | Folder | DLC | Release date |
+|---------|--------|-----|--------------|
+| 80s Downtown Beat | ? | Paid DLC | 2022-11-15 |
+| 80s Movie Tunes | ? | Paid DLC | 2023-03-22 |
+| 90s Pop Radio | ? | Paid DLC | 2023-05-23 |
+| African Vibes | ? | Paid DLC | 2022-12-13 |
+| Jadia Radio | ? | Paid DLC | 2023-03-22 |
+| Piano Tunes Radio | ? | Paid DLC | 2023-05-23 |
+| Pop Punk Radio | ? | Paid DLC | 2023-03-22 |
+
+
+*Hint: The tagged music is also prepared for import into your favourite music player/library, with tags like Album, Artist and Title added.*
+
+### How to use
+First install at least Python 3.9 and the following dependencies:
+- mutagen
+- pandas
+
+Run "`convert_cs1-radio_to_cs2.py`" with options:
+- `-i` or `--input="path"` [default=""]
+  - Path to files to operate on. Wildcards can be applied e.g. -i 'C:\Program Files (x86)\Steam\steamapps\common\Cities_Skylines\Files\Radio\Music\', [default=]."
+- `-o` or `--output="path"` [default="./out/"]
+  -  Path to output of files [default='out\']
+- `-v`
+  - Enable verbose output.
+
+### Example
+> `convert_cs1-radio_to_cs2.py -v -i "C:\Program Files (x86)\Steam\steamapps\common\Cities_Skylines\Files\Radio\Music\"`
+
+### Missing functionality/content
+- A number of Music Pack DLCs (see above)
+- Track numbers for each track
+- Additional meta data for each tack
+- Album art
+- Cities Skylines 2 radio network art/icon
+- Cities Skylines 2 radio channel art/icon
+- Cities Skylines 2 radio channel description
+- Cities Skylines 2 radio channel playlist length
---
+++ convert_cs1-radio_to_cs2.py
@@ -0,0 +1,208 @@
+import json
+import os
+import pandas as pd
+import sys
+import getopt
+import mutagen
+from shutil import copyfile
+from shutil import SameFileError
+
+def getOpts(argv):
+    # Set some output defaults
+    inputPath = ""
+    outputPath = "out"
+    verbose=False
+
+    # Check command line args, and if available, override the defaults
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:v", ["input=", "output=", "ffmpeg="])
+    except getopt.GetoptError:
+        print(r"convert_cs1-radio_to_cs2.py [-i <inputPath>] [-o <outputPath>] [-v]")
+        sys.exit(2)
+
+    # Walk through each option and arg
+    for opt, arg in opts:
+        if opt == '-h':
+            printHelp()
+            sys.exit()
+        elif opt in ("-i", "--input"):
+            inputPath = arg
+        elif opt in ("-o", "--output"):
+            outputPath = arg
+        elif opt in ("-v"):
+            verbose = True
+    
+    # Return final output, either defaults or overridden ones
+    return inputPath, outputPath, verbose
+
+def printHelp():
+    print(r"convert_music_cs1_to_cs2.py [-i <inputPath>] [-o <outputPath>]")
+    print(r"")
+    print(r"Automatically copies and tags CS1 radio for CS2, and creates radio network and channel settings files for each supported station.")
+    print(r"")
+    print(r"Command-line options:")
+    print(r"-i path")
+    print(r"--input=path")
+    print(r"    Path to files to operate on. Wildcards can be applied e.g. -i 'C:\Program Files (x86)\Steam\steamapps\common\Cities_Skylines\Files\Radio\Music\', [default=].")
+    print(r"")
+    print(r"-o path")
+    print(r"--output=path")
+    print(r"    Path to output of files [default='out\'].")
+    print(r"")
+    print(r"-v")
+    print(r"    Enable verbose output.")
+
+def copyAndTagTracks(dfMapping: pd.DataFrame, inputPath: str, outputPath: str, verbose: bool):
+    # Output some diagnostic info if verbose
+    if(verbose): print(f"Converting files in: {inputPath}")
+    if(verbose): print(f"Outputting files to: {outputPath}")
+
+    # Execute operation on each file
+    for index, row in dfMapping.iterrows():
+        # Cache input filepath and output filepath
+        inputFilename = row["Source Filename"]
+        inputAlbum = row["Source Directory"]
+        destFilename = f"{row['Album']} - {row['Artist']} - {row['Title']}.{row['Extension']}"
+        inputAlbumFullPath = os.path.join(inputPath, inputAlbum)
+        inputFileFullPath = os.path.join(inputAlbumFullPath, inputFilename)
+        destFileFullPath = os.path.join(outputPath, destFilename)
+
+        # Debut output
+        if(verbose): print(f"Converting: '{os.path.join(inputAlbum, inputFilename)}' -> '{destFileFullPath}'... ", end="", flush=True)
+
+        # Check for input file
+        if os.path.exists(inputFileFullPath):
+            try:
+                # Copy file
+                copyfile(inputFileFullPath, destFileFullPath)
+
+                # Update tags
+                track = mutagen.File(destFileFullPath)
+                track["album"] = row["Album"]
+                track["radio channel"] = row["Album"]
+                track["artist"] = row["Artist"]
+                track["title"] = row["Title"]
+                track["type"] = "Music"
+                track["radio network"] = "Cities Skylines Classic"
+                track.save()
+
+                #Print success if verbose
+                if(verbose): print(f"Done")
+            
+            # If source and destination are same
+            except SameFileError:
+                print("Source and destination represents the same file.")
+            
+            # If destination is a directory.
+            except IsADirectoryError:
+                print("Destination is a directory.")
+            
+            # If there is any permission issue
+            except PermissionError:
+                print("Permission denied.")
+
+            # For other errors
+            except:
+                print("Error occurred while copying file.")
+        else:
+            # File not found, print diagnostic message
+            if(verbose): print(f"WARN - File Not Found")
+
+def serializeSettingsFile(object, type: str, fileFullPath: str):
+    # Serializing json
+    json_object = json.dumps(object, indent=4)
+    
+    # Writing to sample.json
+    with open(fileFullPath, "w") as outfile:
+        outfile.writelines(type)
+        outfile.write(json_object)
+
+def serlializeSettingsFiles(dfAlbumsData: pd.DataFrame, outputPath: str, verbose: bool):
+    # Prepare and save network JSON settings file
+    network = {
+        "name": "Cities Skylines Classic",
+        "nameId": "Cities Skylines Classic",
+        "description": "Commercial radio stations",
+        "descriptionId": "Cities Skylines Classic",
+        "icon": "Media/Radio/Stations/TEST - Classical.svg",
+        "uiPriority": 2,
+        "allowAds": True
+    }
+    outputFile = os.path.join(outputPath, f"{network['nameId']}.coc")
+    serializeSettingsFile(network, "Radio Network", outputFile)
+    if(verbose): print(f"Serialized network settings file: {outputFile}")
+
+    # Prepare and save radio channel JSON settings files
+    for channelName in dfAlbumsData["Album"]:
+        radioChannel = {
+            "network": network["nameId"],
+            "name": channelName,
+            "description": channelName,
+            "icon": "Media/Radio/Stations/TEST - Pop.svg",
+            "uiPriority": -1,
+            "programs": [
+                {
+                    "name": "Music non stop",
+                    "description": "Dance all day, dance all night",
+                    "icon": "Media/Radio/Stations/TEST - Pop.svg",
+                    "type": "Playlist",
+                    "startTime": "00:00",
+                    "endTime": "00:00",
+                    "loopProgram": True,
+                    "segments": [
+                        {
+                            "type": "Playlist",
+                            "tags": [
+                                "type:Music",
+                                f"radio channel:{channelName}"
+                            ],
+                            "clipsCap": 3
+                        },
+                        {
+                            "type": "Commercial",
+                            "tags": [
+                                "type:Commercial"
+                            ],
+                            "clipsCap": 2
+                        }
+                    ]
+                }
+            ]
+        }
+        outputFile = os.path.join(outputPath, f"{radioChannel['name']}.coc")
+        serializeSettingsFile(radioChannel, "Radio Channel", outputFile)
+        if(verbose): print(f"Serialized radio channel settings file: {outputFile}")
+
+def main(argv):
+    # Get options
+    inputPath, outputPath, verbose = getOpts(argv)
+
+    print(outputPath)
+
+    # Normalize paths
+    inputPath = os.path.normpath(inputPath)
+    outputPath = os.path.normpath(outputPath)
+
+    # If input path does not exist, create it
+    if not os.path.exists(inputPath):
+        print(f"Input path '{inputPath}' does not exist.")
+        sys.exit(2)
+
+    # If output path does not exist, create it
+    if not os.path.exists(outputPath):
+        os.mkdir(outputPath)
+
+    # Read mapping table
+    dfMapping = pd.read_csv("cs1_to_cs2_mapping.csv")
+
+    # Copy and tag all the tracks according to the mappign table
+    copyAndTagTracks(dfMapping, inputPath, outputPath, verbose)
+
+    # Prepare a table of all albums with play times etc (at some point, for now just unique album names)
+    dfAlbumData = dfMapping.drop_duplicates(subset = "Album")
+
+    # Serialize settings files based on album names along with a main network for all Cities Skylines stations
+    serlializeSettingsFiles(dfAlbumData, outputPath, verbose)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
